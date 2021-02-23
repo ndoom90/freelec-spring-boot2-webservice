867df4d404533ea8184e6c28496f9ec041567b2c
ndoom90 <ndoom90@gmail.com>
2021-02-23 22:04:23 +0900

    Done

--- src/main/java/org/example/book/springboot/web/dto/HelloResponseDto.java
+++ src/main/java/org/example/book/springboot/web/dto/HelloResponseDto.java
@@ -4,22 +4,10 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class HelloResponseDto {
 
     private final String name;
     private final int amount;
-
-    public HelloResponseDto(String name, int amount) {
-        this.name = name;
-        this.amount = amount;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public int getAmount() {
-        return this.amount;
-    }
 
 }
