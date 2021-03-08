a315eb4eb336d032ab1735ac472eb8db093be189
ndoom90 <ndoom90@gmail.com>
2021-03-09 01:00:03 +0900

    Add view
    1. Post registration
    2. Whole lookup

--- src/main/java/org/example/book/springboot/web/IndexController.java
+++ src/main/java/org/example/book/springboot/web/IndexController.java
@@ -1,13 +1,25 @@
 package org.example.book.springboot.web;
 
+import lombok.RequiredArgsConstructor;
+import org.example.book.springboot.service.posts.PostsService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 }
