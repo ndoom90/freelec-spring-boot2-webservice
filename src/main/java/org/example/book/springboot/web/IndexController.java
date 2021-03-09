8d590abfb4dab969c3a9dce70fdd78b520f04259
ndoom90 <ndoom90@gmail.com>
2021-03-10 00:07:40 +0900

    Done mustache

--- src/main/java/org/example/book/springboot/web/IndexController.java
+++ src/main/java/org/example/book/springboot/web/IndexController.java
@@ -2,9 +2,11 @@ package org.example.book.springboot.web;
 
 import lombok.RequiredArgsConstructor;
 import org.example.book.springboot.service.posts.PostsService;
+import org.example.book.springboot.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,5 +23,12 @@ public class IndexController {
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
         return "index";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
     }
 }
